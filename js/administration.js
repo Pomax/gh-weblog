fba0268eb8286a4e3bad8ee2277b5edd6a25e0e9
Mike Kamermans <pomax@nihongoresources.com>
2013-10-30 12:31:35 -0700

    missing fallback value

--- js/administration.js
+++ js/administration.js
@@ -177,7 +177,7 @@
    */
   context.setCredentials = function setCredentials(silent) {
     var creds = localStorage["gh-weblog-token"];
-    var newcreds = (silent ? creds : prompt("Please specify your github token" + (creds ? ". Current token: "+creds : '')));
+    var newcreds = (silent ? creds || "undefined": prompt("Please specify your github token" + (creds ? ". Current token: "+creds : '')));
     if(newcreds.trim()=="") { newcreds = "undefined"; }
     localStorage["gh-weblog-token"] = newcreds;
     if(newcreds == "undefined") { document.body.classList.add("default"); }
