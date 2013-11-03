d3c0265b713bac2b5e82c5389a03f2d4138376c5
Mike Kamermans <pomax@nihongoresources.com>
2013-11-03 11:23:32 -0800

    swapped plain markdown for gh-flavour marked.js

--- gh-weblog/js/administration.js
+++ gh-weblog/js/administration.js
@@ -34,7 +34,7 @@ function setupPostHandling() {
 //  console.log("parse entry " + entry.id);
     var content = entry.querySelector(".content"),
         ocontent = entry.querySelector(".original.content");
-    content.innerHTML = markdown.toHTML(content.textContent);
+    content.innerHTML = marked(content.textContent);
     [content, ocontent].forEach(function(e) {
       e.show = function() { e.classList.remove("hidden"); }
       e.hide = function() { e.classList.add("hidden"); }
--- gh-weblog/js/main.js
+++ gh-weblog/js/main.js
@@ -108,5 +108,5 @@ function setupWebLog(options) {
     });
   }
 
-  load(["github.js", "nunjucks.js", "administration.js", "content.js", "markdown.min.js"]);
+  load(["github.js", "nunjucks.js", "administration.js", "content.js", "marked.js"]);
 }
