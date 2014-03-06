ba724fcd3d15d140140222062dbe6254f7ef219a
Pomax <pomax@nihongoresources.com>
2014-03-06 10:32:48 -0800

    .

--- gh-weblog/js/main.js
+++ gh-weblog/js/main.js
@@ -108,5 +108,5 @@ function setupWebLog(options) {
     });
   }
 
-  load(["underscore.js", "octokit.js", "nunjucks.js", "administration.js", "content.js", "marked.js"]);
+  load(["jquery.2", "underscore.js", "octokit.js", "nunjucks.js", "administration.js", "content.js", "marked.js"]);
 }
