721a6ce8783112df79433452cf0662e92fa1d098
Pomax <pomax@nihongoresources.com>
2014-12-29 15:38:07 -0800

    yeahhh

--- gh-weblog/content/fakeconnector.js
+++ gh-weblog/content/fakeconnector.js
@@ -1,9 +1,13 @@
 var Connector = function(options) {
   this.options = options;
+  var octokit = new Octokit({ token: options.token });
+  this.repo = octokit.getRepo(options.user, options.repo);
+  this.branch = this.repo.getBranch(options.branch);
 };
 
 Connector.prototype = {
 
+  // We don't need zepto or jquery for an xhr .get()
   get: function(url, options, processData) {
     if(options && !processData) {
       processData = options;
@@ -19,6 +23,7 @@ Connector.prototype = {
     xhr.send(null);
   },
 
+  // And we also don't need zepto or jquery for an xhr .json()
   json: function(url, processData) {
     this.get(url, { responseType: "json" }, processData);
   },
@@ -46,18 +51,34 @@ Connector.prototype = {
     console.log("saving index");
   },
 
-  saveEntry: function(entry, index) {
+  saveEntry: function(entry, index, saved) {
     var id = entry.state.id;
-    console.log("Saving " + id + " remotely");
+    console.log("Saving " + id);
 
     var metadata = JSON.stringify(entry.getMetaData(), false, 2);
-    console.log(this.options.path + "/content/posts/metadata/" + id + ".json:", metadata)
+    var metadataFilename = this.options.path + "/content/posts/metadata/" + id + ".json";
+
+    console.log(metadataFilename, ":", metadata)
 
     var postdata = entry.getPostData();
-    console.log(this.options.path + "/content/posts/markdown/" + id + ".md:", postdata);
+    var postdataFilename = this.options.path + "/content/posts/markdown/" + id + ".md";
+
+    console.log(postdataFilename + ":", postdata);
 
     var index = JSON.stringify({index:index},false,2);
-    console.log(this.options.path + "/content/index.json", index);
+    var indexFilename = this.options.path + "/content/index.json";
+
+    console.log(indexFilename, index);
+
+    var contents = {};
+    content[indexFilename] = index;
+    content[metadataFilename] = metadata;
+    content[postdataFilename] = postdat;
+
+    this.branch.writeMany(contents, "Saving new entry " + id + " remotely").then(function() {
+      console.log("finished saving files");
+      if(saved) saved();
+    });
   },
 
   updateEntry: function(entry) {
@@ -70,7 +91,6 @@ Connector.prototype = {
     console.log("Deleting " + id + " remotely");
     console.log(this.options.path + "/content/posts/metadata/" + id + ".json")
     console.log(this.options.path + "/content/posts/markdown/" + id + ".md");
-
     var index = JSON.stringify({index:index},false,2);
     console.log(this.options.path + "/content/index.json", index);
   }
@@ -78,9 +98,9 @@ Connector.prototype = {
 };
 
 var connector = new Connector({
+  token: localStorage["gh-weblog-token"],
   user: "Pomax",
   repo: "gh-blog",
   branch: "react",
-  path: "gh-weblog",
-  token: "abcdefghijklmnopqrstuvwxyz"
+  path: "gh-weblog"
 });
