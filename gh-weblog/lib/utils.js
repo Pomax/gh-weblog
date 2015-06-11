4d0fbb7ae52a94dc51f909dc8ee0963e3ef34aa9
Pomax <pomax@nihongoresources.com>
2015-06-11 11:12:20 -0700

    better vanity urls

--- gh-weblog/lib/utils.js
+++ gh-weblog/lib/utils.js
@@ -3,7 +3,7 @@ module.exports = {
    * Clean up a title so that it'll look good as a vanity URL.
    */
   titleReplace: function(title) {
-    return title.replace(/[\s\:;,_.'"\u2010-\u2015]+/g,'-')
+    return title.replace(/[\s\:;,_.'"#!?\u2010-\u2015]+/g,'-')
                 .replace(/-+/g,'-')
                 .replace(/^-/, '')
                 .replace(/-$/, '')
